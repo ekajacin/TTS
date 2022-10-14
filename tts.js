92ef4fbf581d476e4a42e309d3924343d2c907b6
Eka Jacinda Rahmawati <rikkayuki68@gmail.com>
2022-10-14 14:20:51 +0700

    versi 2

--- tts.js
+++ tts.js
@@ -1,58 +1,47 @@
-const btnklick = document.getElementById("submit")
-const inputjwb = document.getElementById("jawab")
+const btnklick = document.getElementById("submit");
+const inputjwb = document.getElementById("jawab");
 
-const M2 = document.querySelectorAll(".m2")
-const M4 = document.querySelectorAll(".m4")
-const M5 = document.querySelectorAll(".m5")
-const M6 = document.querySelectorAll(".m6")
+const M2 = document.querySelectorAll(".m2");
+const M4 = document.querySelectorAll(".m4");
+const M5 = document.querySelectorAll(".m5");
+const M6 = document.querySelectorAll(".m6");
 
+const T1 = document.querySelectorAll(".t1");
+const T3 = document.querySelectorAll(".t3");
+const T2 = document.querySelectorAll(".t2");
 
-const T1 = document.querySelectorAll(".t1")
-const T3 = document.querySelectorAll(".t3")
-const T2 = document.querySelectorAll(".t2")
-
-
-btnklick.addEventListener('click', function(){
-    let soal = document.getElementById("pilih").value;
-    let jwbn = inputjwb.value.toLowerCase();
-    // alert(jwbn);
-    console.log(soal);
-   if (soal === '1'&& jwbn === 'takjil') {
-    alert("jawaban anda benar");
+btnklick.addEventListener("click", function () {
+  let soal = document.getElementById("pilih").value;
+  let jwbn = inputjwb.value.toLowerCase();
+  if (soal === "1" && jwbn === "takjil") {
     for (let i = 0; i < T1.length; i++) {
-        T1[i].innerHTML = jwbn[i]
+      T1[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '2'&& jwbn === 'tadarus') {
-    alert("jawaban anda benar");
+  } else if (soal === "2" && jwbn === "tadarus") {
     for (let i = 0; i < T2.length; i++) {
-        T2[i].innerHTML = jwbn[i]
+      T2[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '3'&& jwbn === 'idul fitri') {
-    alert("jawaban anda benar");
+  } else if (soal === "3" && jwbn === "idulfitri") {
     for (let i = 0; i < T3.length; i++) {
-        T3[i].innerHTML = jwbn[i]
+      T3[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '4'&& jwbn === 'ied') {
-    alert("jawaban anda benar"); 
+  } else if (soal === "4" && jwbn === "tarawih") {
     for (let i = 0; i < M2.length; i++) {
-        M2[i].innerHTML = jwbn[i]
+      M2[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '5'&& jwbn === 'ramadhan') {
-    alert("jawaban anda benar");
+  } else if (soal === "5" && jwbn === "ramadan") {
     for (let i = 0; i < M4.length; i++) {
-        M4[i].innerHTML = jwbn[i]
+      M4[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '6'&& jwbn === 'nafsu') {
-    alert("jawaban anda benar");
+  } else if (soal === "6" && jwbn === "haus") {
     for (let i = 0; i < M5.length; i++) {
-        M5[i].innerHTML = jwbn[i]
+      M5[i].innerHTML = jwbn[i];
     }
-   } else if (soal === '7'&& jwbn === 'ngabuburit') {
-    alert("jawaban anda benar");
+  } else if (soal === "7" && jwbn === "nabuburit") {
     for (let i = 0; i < M6.length; i++) {
-        M6[i].innerHTML = jwbn[i]
+      M6[i].innerHTML = jwbn[i];
     }
-   } 
-   
-}
-)
+  } else {
+    alert("jawaban anda salah");
+  }
+});
